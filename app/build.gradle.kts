a9e73ba088fcbf4e6ba438c0f3c43c03463f048e
hotleen <927499561s@gmail.com>
2025-06-02 09:28:26 +0800

    feat: Config for Android Compose
    This commit introduces the initial setup for the Android application "ThinkLab".
    
    Key changes include:
    - Added necessary Gradle configuration files (build.gradle.kts, settings.gradle.kts, gradle.properties, gradle-wrapper.*).
    - Configured app module (app/build.gradle.kts) with namespace, SDK versions, build types, and dependencies.
    - Added AndroidManifest.xml with basic application setup.
    - Included default resource files (colors.xml, strings.xml, themes.xml, backup_rules.xml, data_extraction_rules.xml).
    - Added launcher icons (ic_launcher*) for different densities and API levels.
    - Set up .gitignore files for both the project root and the app module.
    - Added Proguard rules (proguard-rules.pro).
    - Included example unit and instrumented tests.
    - Configured repositories in settings.gradle.kts to use Aliyun mirrors for faster dependency resolution.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -26,6 +27,12 @@ android {
             )
         }
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+//        kotlinCompilerExtensionVersion = "1.6.0"
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -37,10 +44,22 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
